29980b6b619cd1259ce9683a7589eaf8a7a5a612
kang <61167784+kang-jp@users.noreply.github.com>
2024-02-16 20:26:47 +0900

    prototype
    first prototype of strange clocks

---
+++ main.js
@@ -0,0 +1,36 @@
+function getUnitTime(){
+  var r = Math.random();
+  if(r < (1/4)){
+    return 1000 * 0
+  }else if(r < (3/4)){
+    return 1000 * 1
+  }else{
+    return 1000 * 2
+  }
+}
+
+
+
+function advanceClock(date){
+  var twoDigit = function (num) {
+    var digit
+    if( num < 10 ){ 
+      digit = "0" + num; 
+    }else { 
+      digit = num; 
+    }
+    return digit;
+  }
+
+  date.setTime(date.getTime()+getUnitTime());
+
+  var hour = twoDigit(date.getHours());
+  var minute = twoDigit(date.getMinutes());
+  var second = twoDigit(date.getSeconds());
+
+  document.getElementById("strange_clock_time").textContent = hour + ":" + minute + ":" + second;
+}
+
+strange_date = new Date();
+
+setInterval(advanceClock, 1000, strange_date);
