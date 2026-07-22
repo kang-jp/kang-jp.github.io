fb3e35e45cc80cc0fae795553af44b247403f91d
kang <61167784+kang-jp@users.noreply.github.com>
2026-07-22 17:36:20 +0900

    update hp latest

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,23 @@ const App = () => {
   return (
     <main className="page">
         kang-jp's HP 
+        <h1>ITO Kango</h1> 
+        <h2>Shelf</h2> 
+        <ul>
+            <li><a href="https://kang-jp.github.io/prototype-of-hp/pdfs/20241221.pdf">
+              "圏論で見る準同型定理"</a>
+              北大オムニバス発表および圏論アドカレ2024day21
+            </li>
+            <li><a href="https://kang-jp.github.io/prototype-of-hp/pdfs/20240728.pdf">
+              "Ind化の話"</a>
+              loc.pres.cat.とalg.cat.のガブリエルウルマー双対について.
+              圏論オンラインゼミの資料.
+            </li>
+        </ul>
+        <h2>Garage</h2>
+        <ul>
+            Timer
+        </ul>
     </main>
   );
 };
